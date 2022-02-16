5e37984b7414bdbf35115430fad6bd4152d805ad
José Carlos <jcolscript@gmail.com>
2022-02-15 23:14:27 -0300

    chore:  config graphql using codegen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
+import { usePageQuery } from "../shared/graphql/generated/graphql"
+
 export default function Home() {
+
+  const [{ data }] = usePageQuery({
+    variables: {
+      slug: "home",
+    },
+  });
+
   return (
-    <h1>Hello World</h1>
+    <h1>{data?.page.title}</h1>
   )
 }
